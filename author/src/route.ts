7a1c4c70260cf92eeb39482ce87f685bed450406
qianrenni <3092176591@qq.com>
2026-04-28 14:45:43 +0800

    feat(app): 实现动态页面标题功能
    - 在 author 和 user 应用中添加路由守卫设置页面标题
    - 为所有路由配置 meta.title 属性
    - 创建 useTitle 工具函数用于动态设置文档标题
    - 在书籍详情和阅读页面中根据内容动态更新标题
    - 集成 useTitle 到共享包中供多应用使用

--- author/src/route.ts
+++ author/src/route.ts
@@ -15,16 +15,19 @@ const routs = [
         path: '/my-book',
         name: 'MyBook',
         component: () => import('./views/book/MyBook.vue'),
+        meta: { title: '我的书' },
       },
       {
         path: '/draft-manage',
         name: 'DraftManage',
         component: () => import('./views/book/DraftManage.vue'),
+        meta: { title: '草稿管理' },
       },
       {
         path: '/account-setting',
         name: 'AccountSetting',
         component: () => import('./views/navigation/AccountSetting.vue'),
+        meta: { title: '账户设置' },
       },
       // {
       //     path: '/system-setting',
@@ -35,6 +38,7 @@ const routs = [
         path: '/book-detail/:id',
         name: 'BookDetail',
         component: () => import('./components/book/BookDetail.vue'),
+        meta: { title: '书籍详情' },
       },
       {
         path: '/book-edit',
@@ -46,16 +50,19 @@ const routs = [
           chapterId: 'chapterId',
         },
         component: () => import('./views/book/BookEdit.vue'),
+        meta: { title: '书籍编辑' },
       },
       {
         path: '/create-book',
         name: 'CreateBook',
         component: () => import('./views/book/CreateBook.vue'),
+        meta: { title: '创建书籍' },
       },
       {
         path: '/edit/book-meta',
         name: 'BookInfoEdit',
         component: () => import('./views/book/BookInfoEdit.vue'),
+        meta: { title: '书籍信息编辑' },
         query: { id: 'id', hasDraft: 'hasDraft' },
       },
     ],
@@ -64,6 +71,7 @@ const routs = [
     path: '/login',
     name: 'Login',
     component: () => import('./views/auth/LoginView.vue'),
+    meta: { title: '登录' },
   },
 ];
 export const router = createRouter({
